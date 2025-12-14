e67f206eb2ed5be59f415535b2239d822ad7e4d3
Kunal Sarpe <42830251+kunalsrp5@users.noreply.github.com>
2025-12-15 00:02:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ layout="wide"
 def get_connection():
     return snowflake.connector.connect(
         user=st.secrets["snowflake"]["user"],
-        password=st.secrets["snowflake"]["password"]
-        account=st.secrets["snowflake"]["account"]
-        warehouse=st.secrets["snowflake"]["warehouse"]
-        database=st.secrets["snowflake"]["database"]
+        password=st.secrets["snowflake"]["password"],
+        account=st.secrets["snowflake"]["account"],
+        warehouse=st.secrets["snowflake"]["warehouse"],
+        database=st.secrets["snowflake"]["database"],
         schema=st.secrets["snowflake"]["schema"]
     )
 
